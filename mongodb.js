b5af0a9d4ff09582f89496db46f66640bc994ba4
Luis Mazariego <mazariego2011@gmail.com>
2020-12-11 21:34:02 -0600

    object id

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,13 @@
 // CRUD operations
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'; //it is best to use ip address instead of localhost.
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -13,26 +16,26 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Do the laundry',
-            completed: false
-        },
-        {
-            description: 'Do the dishes',
-            completed: false
-        },
-        {
-            description: 'Go to work',
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert tasks!');
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Do the laundry',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Do the dishes',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Go to work',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks!');
+    //     }
 
-        console.log(result.ops);
-    });
+    //     console.log(result.ops);
+    // });
 
     // db.collection('users').insertOne({
     //     name: 'Luis',
