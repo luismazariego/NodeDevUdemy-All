d0445ce0601c51a7ede1ee9a6cb4bbbbbcb77a01
Luis Mazariego <luis@thresholdcommunications.com>
2020-12-14 09:26:18 -0600

    delete and update

--- mongodb.js
+++ mongodb.js
@@ -6,8 +6,8 @@ const connectionUrl = 'mongodb://127.0.0.1:27017'; //it is best to use ip addres
 const databaseName = 'task-manager';
 
 const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
+// console.log(id);
+// console.log(id.getTimestamp());
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -15,6 +15,73 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName);
+
+    db.collection('tasks').deleteOne({
+        description: 'Go to work'
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err)
+    });
+
+    // db.collection('users').deleteMany({
+    //     age: 31
+    // }).then((res) => {
+    //     console.log(res)
+    // }).catch((err) => {
+    //     console.log(err)
+    // })
+
+    //#region Examples
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((res) => {
+    //     console.log(res);
+    // }).catch((err) => {
+    //     console.log(err);
+    // });
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("5fd77eb5bcb61b19125f6591")
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((res) => {
+    //     console.log(res);
+    // }).catch((error) => {
+    //     console.log(error);
+    // });
+    // db.collection('tasks').findOne({ _id: new ObjectID("5fd7623a73a0c52d9c66723c") }, (err, tasks) => {
+    //     if (err) {
+    //         console.log('Unable to fetch');
+    //     }
+
+    //     console.log(tasks);
+    // })
+
+    // db.collection('tasks').find({ completed: false }).toArray((err, tasks) => {
+    //     if (err) {
+    //         console.log('Unable to fetch');
+    //     }
+
+    //     console.log(tasks);
+    // });
+
+    // db.collection('users').findOne({ _id: new ObjectID("5fd762211d9bef1254d0b8c6") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch');
+    //     }
+
+    //     console.log(user);
+    // });
+
+    // db.collection('users').find({ age: 30 }).toArray((error, users) => {
+    //     console.log(users);
+    // });
 
     // db.collection('tasks').insertMany([
     //     {
@@ -63,4 +130,5 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
 
     //     console.log(result.ops);
     // });
+    //#endregion
 });
